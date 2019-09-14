414b23af24708d11ae310b1ad92128ac8fb66ec8
jay depala <jaidepala@github.com>
2019-09-15 00:44:28 +0530

    added build

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
 
 app.listen(port);
