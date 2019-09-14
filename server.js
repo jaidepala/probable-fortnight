5ebbd2c0abb303adda93ea174b08d3b62318acb9
jay depala <jaidepala@github.com>
2019-09-15 01:07:44 +0530

    followed itnext

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
+//Install express server
 const express = require('express');
-
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/client/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/client/dist/client'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
+
+    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
 });
 
-// Start the app by listening on the default Heroku port    
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
