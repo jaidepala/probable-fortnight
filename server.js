5299909aedcbed031d19928481389d8ae8c69db1
jay depala <jaidepala@github.com>
2019-09-15 00:50:29 +0530

    changed server script

--- server.js
+++ server.js
@@ -1,22 +1,15 @@
 const express = require('express');
+
 const path = require('path');
-const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.port || 8080;
 
-// (optional) only made for logging and
-// bodyParser, parses the request body to be a readable json format
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// app.use(logger("dev"));
+// Serve only the static files form the dist directory    
+app.use(express.static(__dirname + '/client/dist'));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/dist/client/index.html'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
-app.listen(port);
-
-console.log(`Password generator listening on ${port}`);
+// Start the app by listening on the default Heroku port    
+app.listen(process.env.PORT || 8080);
