4cca99dc5d98b00201e8407fbb4839f86316f633
jay depala <jaidepala@github.com>
2019-09-15 19:58:01 +0530

    task 2 complete

--- client/src/app/modules/task1/task1/task1.component.ts
+++ client/src/app/modules/task1/task1/task1.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-task1',
-  templateUrl: './task1.component.html',
-  styleUrls: ['./task1.component.css']
+    selector: 'app-task1',
+    templateUrl: './task1.component.html',
+    styleUrls: ['./task1.component.css']
 })
 export class Task1Component implements OnInit {
 
-  constructor() { }
+    public arr = [];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+
+        for(let i = 1; i <= 100; i++)
+        {
+            let str = i.toString();
+            if(i % 3 == 0 || i % 5 == 0)
+            {
+                if(i % 3 == 0 && i % 5 != 0)
+                {
+                    str = 'Fizz';
+                }
+                else if(i % 3 != 0 && i % 5 == 0)
+                {
+                    str = 'Buzz';
+                }
+                else
+                {
+                    str = 'FizzBuzz';
+                }
+            }
+
+            this.arr.push( str );
+        }
+    }
 
 }
--- client/src/app/modules/task2/task2/task2.component.ts
+++ client/src/app/modules/task2/task2/task2.component.ts
@@ -1,15 +1,187 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from "@angular/core";
+import { Observable, of, throwError as observableThrowError } from 'rxjs';
+// import { of } from "rxjs";
+import {
+  debounceTime,
+  map,
+  distinctUntilChanged,
+  filter
+} from "rxjs/operators";
+import { fromEvent } from 'rxjs';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 
 @Component({
-  selector: 'app-task2',
-  templateUrl: './task2.component.html',
-  styleUrls: ['./task2.component.css']
+    selector: 'app-task2',
+    templateUrl: './task2.component.html',
+    styleUrls: ['./task2.component.css']
 })
 export class Task2Component implements OnInit {
 
-  constructor() { }
+    public allCharacters = [];
+    public sortedCharacters = [];
+    public filteredData = [];
+    public url = 'https://swapi.co/api/people/?format=json';
+    public pageNo = 0;
+    public isButtonDisabled = false;
 
-  ngOnInit() {
-  }
+    @ViewChild('searchCharacter', { static: true }) searchCharacter: ElementRef;
 
+    constructor(
+        private http: HttpClient,
+        private snackBar: MatSnackBar
+    ) { }
+
+    ngOnInit() {
+
+        this.getData();
+
+        /*
+            !   REF
+            *    
+            *   https://www.freakyjolly.com/angular-7-6-add-debounce-time-using-rxjs-6-x-x-to-optimize-search-input-for-api-results-from-server/
+        */
+        fromEvent(this.searchCharacter.nativeElement, 'keyup')
+            .pipe(
+                // get value
+                map((event: any) => {
+                    return event.target.value;
+                }),
+                // if character length greater then 1
+                // , filter(res => res.length > 1)
+                // Time in milliseconds between key events
+                debounceTime(1000),
+                // If previous query is diffent from current   
+                distinctUntilChanged()
+                // subscription for response
+            )
+            .subscribe((text: string) => {
+
+                if (text && text != null)
+                    this.filteredData = this.filterBy('name', text, 'matches');
+                else
+                    this.filteredData = this.allCharacters;
+            });
+    };
+
+    public getData() {
+
+        this.isButtonDisabled = true;
+
+        this.getAllCharacters()
+            .subscribe((result) => {
+
+                if( result && result != null )
+                {
+                    this.isButtonDisabled = result.next ? false : true;
+
+                    let thisResult = result.results.map((thisResult) => {
+
+                        thisResult['birthYear'] = this.getBirthYear(thisResult.birth_year);
+
+                        return thisResult;
+                    });
+
+                    this.allCharacters = this.allCharacters.concat(thisResult);
+
+                    if (this.searchCharacter.nativeElement && this.searchCharacter.nativeElement.value && this.searchCharacter.nativeElement.value != null)
+                        this.filteredData = this.filterBy('name', this.searchCharacter.nativeElement.value, 'matches');
+                    else
+                        this.filteredData = this.allCharacters;
+
+                    let copiedData = this.allCharacters.map(x => Object.assign({}, x));;
+
+                    this.sortedCharacters = this.sortAgeWise( copiedData )
+                }
+
+            }, (err) => {
+
+            });
+    };
+
+    public getAllCharacters(): Observable<any> {
+        return this.http.get(this.getUrl());
+    };
+
+    public getUrl(): string {
+
+        this.pageNo++;
+
+        return this.url + '&page=' + this.pageNo;
+    };
+
+    public filterBy( filterKey: string, filterValue: any, matchBy: string = 'equal' ) {
+        return this.allCharacters.filter(( thisCharacter ) => {
+
+            if (thisCharacter[filterKey] && filterValue )
+            {
+                if( matchBy == 'equal' )
+                {
+                    return thisCharacter[filterKey] == filterValue;
+                }
+                else if( matchBy == 'lessthan' )
+                {
+                    return thisCharacter[filterKey] <= filterValue;
+                }
+                else if( matchBy == 'greaterthan' )
+                {
+                    return thisCharacter[filterKey] >= filterValue;
+                }
+                else if( matchBy == 'matches' )
+                {
+                    let theValue = thisCharacter[filterKey].toString().toLowerCase(),
+                        matchingValue = filterValue.toString().toLowerCase();
+
+                    if(theValue.indexOf( matchingValue ) > -1)
+                        return thisCharacter;
+                }
+            }
+        });
+    };
+
+    public getAverageOf( theKey: string) {
+
+        let average = 0;
+
+        this.allCharacters.map(( thisCharacter ) => {
+
+            let thisKeyValue = thisCharacter[theKey];
+
+            if( thisKeyValue && !isNaN(thisKeyValue) )
+                average += parseInt( thisKeyValue );
+            
+            // console.log('thisCharacter['+ theKey +']', thisKeyValue, average);
+        });
+
+        return Math.round(average/this.filteredData.length);
+    };
+
+    public getBirthYear(birthYear: string): number {
+
+        let calculatedBirthYear = 0;
+
+        if( birthYear && !isNaN( parseFloat(birthYear) ) )
+            calculatedBirthYear = parseFloat(birthYear);
+
+        return calculatedBirthYear;
+    };
+
+    public sortAgeWise(data) {
+        return data.sort(( aChar, bChar ) => {
+
+            let aCharAge = aChar.birthYear,
+                bCharAge = bChar.birthYear;
+
+            if( aCharAge > bCharAge )
+                return 1;
+            else if( aCharAge < bCharAge )
+                return -1;
+
+            return 0;
+        });
+    };
 }
